75e973fcfda3abbe620ccf4194f2d53e58b621d1
Siphamandla Zolakhe <zwelakhespha@gmail.com>
2024-08-22 00:04:29 +0200

    Implemented Logging with Serilog and Seq

---
+++ BookShelve.Api/Static/Messages.cs
@@ -0,0 +1,11 @@
+﻿namespace BookShelve.Api.Static
+{
+    public static class Messages
+    {
+        public const string Error500Message = "There was an issue completing this request. Please try again later.";
+        public const string SuccessMessage = "Successfully read and returned data from the database.";
+        public const string Success204Message = "Successfully updated data from the database.";
+        public const string Success201Message = "Successfully record new information in the database";
+        public const string DeleteResourceMessage = "Record(s) has been deleted from the database.";
+    }
+}
